5376ff91f6cb4c4b0b6dc2a60d22ffa32e73d23f
John Desrosiers <johndrunner@yahoo.com>
2017-08-29 21:15:44 -0700

    added several functions. still wip

--- assets/javascript/rpsGame.js
+++ assets/javascript/rpsGame.js
@@ -1,79 +1,82 @@
-/*
-*	----- Game -----
-*/
-var game = {
+var playerNum;
 
+
+// initialize firebase
+var config = {
+    apiKey: "AIzaSyBcw7vxfVodOnXmmesBCNFN625DR_CTvdY",
+    authDomain: "rps-multi-e7e39.firebaseapp.com",
+    databaseURL: "https://rps-multi-e7e39.firebaseio.com",
+    projectId: "rps-multi-e7e39",
+    StorageBucket: "rps-multi-e7e39.appspot.com",
+    messagingSenderId: "880645929001"
 };
+firebase.initializeApp(config);
 
-/*
-*	----- storage -----
-*
-* 	Provides methods and properties for saving and retrieving game
-*	data.
-*	
-*/
-var storage = {
-	// --- values --- //
-	playerCount: 0,
+// database references
+var database = firebase.database();
+var fbRefPlayers = database.ref("/players");
+var fbRefUser;
+var fbRefOpponent;
 
-	// --- refernces --- //
-	playersRef: false,
-	userRef: false,
-	opponentRef: false,
+// setup references to the players
+fbRefPlayers.once("value", function(snap) {
+
+	// no players yet; player 1
+	if ( !snap.exists() ) {
+		fbRefUser = fbRefPlayers.child(1);
+		fbRefOpponent = fbRefPlayers.child(2);
+		playerNum = 1;
+	} else if ( snap.numChildren() === 1 ) {
+		fbRefOpponent = fbRefPlayers.child(1);
+		fbRefUser = fbRefPlayers.child(2);
+	} else {
+		console.log("Already 2 players.");
+	}
+
+	// listen for changes to the opponent data
+	fbRefOpponent.on("value", function(snap) {
+
+		// update the opponent name
+
+	});
 
 
-	// --- methods --- //
+});
 
-	// Adds player to database if possible.
-	addPlayer: function(player) {
-		
-		if ( this.playerCount === 0 ) {
-			// add player to database as player 1
-			this.userRef = this.playersRef.child(1);
-		} else if ( !this.userRef && this.playerCount === 1){
-			// add player to db as player 2
-			this.userRef = this.playersRef.child(2);
 
-		// return 0 if game is full and player not already
-		// in the game
-		} else if ( !this.userRef ) {
-			console.log("storage: Already 2 players.");
-			return 0;
+
+// Handles a change in the opponent.
+function opponentChanged(opp) {
+
+	// render the name
+
+}
+
+// Handles a change in the user.
+function userChanged(user) {
+
+	// update the database
+
+	// update the view
+	view.renderStart(user.name, 1, "Waiting for opponent to join.");
+
+}
+
+var players = function() {
+
+	var user = new Player();
+	var opponent = new Player();
+
+
+	return {
+		opponent: {
+
+		},
+		user: {
+
 		}
-		this.userRef.set(player);
-		return this.userRef.key;
-	},
-	setOpponentRef: function(key) {
-
-	},
-	connect: function() {
-
-		// initialize the database connection
-		var config = {
-		    apiKey: "AIzaSyBcw7vxfVodOnXmmesBCNFN625DR_CTvdY",
-		    authDomain: "rps-multi-e7e39.firebaseapp.com",
-		    databaseURL: "https://rps-multi-e7e39.firebaseio.com",
-		    projectId: "rps-multi-e7e39",
-		    StorageBucket: "rps-multi-e7e39.appspot.com",
-		    messagingSenderId: "880645929001"
-		};
-		firebase.initializeApp(config);
-
-		// database references
-		var database = firebase.database();
-		this.playersRef = database.ref("/players");
-
-		// handle change in players
-		this.playersRef.on("value", function(snap) {
-			storage.playerCount = snap.numChildren();
-			console.log(snap.exportVal());
-		});
-	},
-	clearAll: function() {
-		this.playersRef.remove();
 	}
 };
-
 
 /*
 *	----- Player -----
@@ -88,24 +91,74 @@ var Player = function(name = "", wins = 0, losses =0 ) {
 
 
 $(document).ready(function() {
-	storage.connect();
+
+	// start button click
 	$("#btnStart").on("click", function() {
+
+		// get players name and reset form
 		var  name = $("#txtName").val().trim();
 		$("#txtName").val("");
+		userChanged(new Player(name));
 
-		var playerNum = storage.addPlayer(new Player(name));
-		console.log(playerNum);
+
+
 	});
 	
 });
 
 /*
-*	----- View -----
+*	----- view -----
 *
 *	Container for rendering dynamic page content.
 */
-var View = {
+var view = {
+	// --- view components --- //
+
+
+	// --- methods --- //
 	alert: function (message) {
 		alert(message);
+	},
+	render: function() {
+
+	},
+	renderStart: function(name, num, status) {
+
+		// fade out greeting and form
+		$(function() {
+			$("#signon, #greeting, #status").fadeOut(400, function() {
+				// make greeting visible
+				$("#greeting").removeClass("invisible");
+
+				// update values
+				view.updatePlayer({
+					name: name,
+					wins: 0,
+					losses: 0,
+					num: num
+				});
+				$("#status").text(status);
+
+				// fade greating, status, and the rest of the game in
+				$(".chat-display, .gameArea .invisible, #greeting, #status")
+					.css("opacity", "0")
+					.show()
+					.removeClass("invisible hidden")
+					.fadeTo(200, 1);
+			});
+		});
+	},
+	renderOpponent: function(opponent) {
+
+	},
+	updatePlayer: function(data) {
+		$(".playerName").text(data.name);
+		$("#playerWins").text(data.wins);
+		$("#playerLosses").text(data.losses);
+		$("#playerNum").text(data.num);
 	}
 };
+
+
+// --- test code --- //
+function fbRemPlayers() { fbRefPlayers.remove(); }
